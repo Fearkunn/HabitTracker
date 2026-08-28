e321a2e97620b7cdaffbeaf17d4de815a14d7287
Fearkunn <richiedaryl@gmail.com>
2026-08-28 14:07:53 +0700

    Initial Commit

---
+++ HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Richie Daryl Kwenandar on 28/08/26.
+//
+
+import Testing
+@testable import HabitTracker
+
+struct HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
